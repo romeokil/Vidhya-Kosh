0ae1e8d924b83103fb4c529d6951462cde53778b
romeokil <rahulkumarjha58978@gmail.com>
2025-10-28 23:01:47 +0530

    course routes and controller roughly completed

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import User from './models/User.js'
 import Instructor from './models/Instructor.js';
+import Course from './models/Course.js';
 const app=express();
 dotenv.config();
 const PORT=8000;
@@ -114,7 +115,7 @@ app.post('/update',async(req,res)=>{
         password,
         role
     })
-
+    await updatedUser.save();
     return res.status(201).json({
         "message":"User updated Successfully!!",
         updatedUser
@@ -211,12 +212,100 @@ app.post('/updateInstructor',async(req,res)=>{
         bio,
         rating
     })
+    await updateInstructor.save();
     return res.status(201).json({
         "message":"Instructor Updated Successfully!!",
         updateInstructor
     })
 })
 
+// Course routes
+
+// register Course
+
+app.post('/registerCourse',async(req,res)=>{
+    const {name,description,price}=req.body;
+    if(!name || !description || !price){
+        return res.status(201).json({
+            "message":"Sorry Missing credentials!!"
+        })
+    }
+    const alreadyregisteredCourse=await Course.findOne({name});
+    if(alreadyregisteredCourse){
+        return res.status(201).json({
+            "message":"Course with this name is already registered!!"
+        })
+    }
+    else{
+        const registerCourse=await Course.create({
+            name,
+            description,
+            price
+        })
+        registerCourse.save();
+        return res.status(201).json({
+            "message":"Course Successfully Registered!!"
+        })
+    }
+})
+
+// get all courses.
+
+app.get('/getallCourses',async(req,res)=>{
+    const getallCourses=await Course.find({});
+    if(!getallCourses){
+        return res.status(401).json({
+            "message":"Sorry Till now No course Registered!!"
+        })
+    }
+    else{
+        return res.status(201).json({
+            "message":"All Courses retrieved!!",
+            getallCourses
+        })
+    }
+})
+
+// update course
+
+app.post('/updateCourse',async(req,res)=>{
+    const {name,description,price}=req.body;
+
+    const isCourse=await Course.findOne({name});
+    if(!isCourse){
+        return res.status(401).json({
+            "message":"Sorry No course with this name"
+        })
+    }
+    else{
+        const updatedCourse=isCourse.updateOne({
+            name,
+            description,
+            price
+        })
+        await updatedCourse.save();
+        return res.status(201).json({
+            "message":"Course updated Successfully!!"
+        })
+    }
+})
+
+app.post('/deleteCourse',async(req,res)=>{
+    const {name}=req.body;
+    const newname=name.toLowerCase();
+    const isCourse=await Course.findOne({newname});
+    if(!isCourse){
+        return res.status(401).json({
+            "message":"Sorry Can't delete the record with this name"
+        })
+    }
+    else{
+        await isCourse.deleteOne();
+        res.status(201).json({
+            "message":"Course successfully Deleted!!"
+        })
+    }
+})
 
 
 app.listen(PORT,async()=>{
