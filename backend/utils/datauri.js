e1f08da6922454219d7731d5d94cf2f60882e9a1
romeokil <rahulkumarjha58978@gmail.com>
2025-11-07 12:48:49 +0530

    cloudinary setup complete ab jo bhi controller me chahiye usme use kr lo.

---
+++ backend/utils/datauri.js
@@ -0,0 +1,11 @@
+import DataUriParser from "datauri/parser.ts"
+
+import path from "path";
+
+const getDataUri = (file) => {
+    const parser = new DataUriParser();
+    const extName = path.extname(file.originalname).toString();
+    return parser.format(extName, file.buffer);
+}
+
+export default getDataUri;
